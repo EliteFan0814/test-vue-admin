32290bb46301c0daab99f1e7b201c7864bcd4cd3
elite fan <fan552426811@outlook.com>
2019-07-02 18:06:08 +0800

    finished

--- src/store/router/index.js
+++ src/store/router/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash'
 
-import {adminRoutes,normalRoues} from '@/mock/router'
+import { adminRoutes, normalRoues } from '@/mock/router'
 
 const state = {
     // 初始化 要加入的 权限路由
@@ -9,19 +9,19 @@ const state = {
 
 const mutations = {
     // 设置 路由表
-    SET_ROUTERS:function(state,adds){
+    SET_ROUTERS: function (state, adds) {
         state.addRoutes.length = 0
-        state.addRoutes = _.concat(state.addRoutes,adds)
+        state.addRoutes = _.concat(state.addRoutes, adds)
     }
 };
 
 const actions = {
     // commit 设置路由表 并 获取路由表
-    getRoutes:function({commit},roles){
+    getRoutes: function ({ commit }, roles) {
         if (_.indexOf(roles, 'admin') !== -1) {
-            commit('SET_ROUTERS',adminRoutes())
+            commit('SET_ROUTERS', adminRoutes())
         } else {
-            commit('SET_ROUTERS',normalRoues())
+            commit('SET_ROUTERS', normalRoues())
         }
     }
 }
